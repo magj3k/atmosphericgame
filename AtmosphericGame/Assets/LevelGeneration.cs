d2ee80024b1faa30bbd9e5b6342da6697715e260
Magnus Johnson <magnus@magmhj.com>
2019-05-27 17:05:00 -0600

    Added simple gameplay test

---
+++ AtmosphericGame/Assets/LevelGeneration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour {
+    public GameObject tilePrefab;
+
+    // Start is called before the first frame update
+    void Start() {
+        var tileSize = tilePrefab.GetComponent<BoxCollider2D>().size.x;
+
+        for (int i = 0; i < 15; i++) {
+            var position = new Vector2((i-7)*tileSize, -2*tileSize);
+            Instantiate(tilePrefab, position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
